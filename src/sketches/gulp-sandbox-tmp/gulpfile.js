c822a59cb0c3767f5ad788950bedcbccd7ea5555
Tim <tymonr@yandex.ru>
2020-09-11 21:50:27 +0300

    gulp v1

---
+++ src/sketches/gulp-sandbox-tmp/gulpfile.js
@@ -0,0 +1,68 @@
+// general
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+
+// preprocessors
+const sourcemaps = require('gulp-sourcemaps');
+const babel = require('gulp-babel');
+const less = require('gulp-less');
+
+// webserver
+const connect = require('gulp-connect');
+
+function clean(cb) {
+	cb();
+}
+
+function css(cb) {
+	gulp
+		.src('./src/**/*.less')
+		.pipe(sourcemaps.init())
+		.pipe(less())
+		.pipe(concat('index.css'))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('dist'));
+
+	cb();
+}
+
+function javascript(cb) {
+	gulp
+		.src('src/**/*.js')
+		.pipe(sourcemaps.init())
+		.pipe(
+			babel({
+				presets: ['@babel/env']
+			})
+		)
+		.pipe(concat('index.js'))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('dist'));
+
+	cb();
+}
+
+function html(cb) {
+	gulp.src('src/**/*.html').pipe(gulp.dest('dist'));
+
+	cb();
+}
+
+function build(cb) {
+	gulp.series(clean, gulp.parallel(css, javascript, html))();
+
+	cb();
+}
+
+function watch(cb) {
+	connect.server({
+		root: 'dist',
+		livereload: true
+	});
+
+	gulp.watch(['src/**/*.js', 'src/**/*.less', 'src/**/*.html'], build);
+
+	cb();
+}
+
+exports.default = gulp.parallel(build, watch);
